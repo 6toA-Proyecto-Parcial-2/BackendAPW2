22a7ac73978bdb3e6f9a1286ccd086b2f2d95b9a
CuencaJ <jersonstevencuencaholguin@gmail.com>
2023-12-10 21:21:46 -0500

    Jorge

--- backend/src/domain/index.ts
+++ backend/src/domain/index.ts
@@ -20,3 +20,14 @@ export * from './entities/traduccionIdioma.entity';
 export * from './repositories/origenIdioma.repository';
 export * from './repositories/idioma.repository';
 export * from './repositories/traduccionIdioma.repository';
+
+export * from './datasources/autor.datasource';
+export * from './datasources/genero.datasource';
+export * from './datasources/nacionalidad.datasource';
+export * from './dtos';
+export * from './entities/autor.entity';
+export * from './entities/genero.entity';
+export * from './entities/nacionalidad.entity';
+export * from './repositories/autor.repository';
+export * from './repositories/genero.repository';
+export * from './repositories/nacionalidad.repository';
